9ecc7631d67150903423276659942390bbb8cf78
Mariana <marianalimajanuario@gmail.com>
2023-02-25 14:58:39 -0300

    :heavy_plus_sign: feat: Aula 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'mariana';
+  title: string;
+  togglePokemon: boolean;
+
+  constructor(){
+    this.title = "Mariana";
+    this.togglePokemon = true;
+  }
 }
